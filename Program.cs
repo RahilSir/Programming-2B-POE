7d98e047f6d5fb0df8cbca36e76bbe2677cff9ba
RahilSir <132093553+RahilSir@users.noreply.github.com>
2024-10-30 21:45:37 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+using CMCS.Data;
+using CMCS.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))); // Use your database provider
+new MySqlServerVersion(new Version(8, 0)); // Specify the version of MySQL you are using
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
